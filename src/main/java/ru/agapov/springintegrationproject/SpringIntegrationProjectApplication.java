75f0df753f0801269308f07b66e2eca4e8af07b3
AgapAnd <braniganzepp@gmail.com>
2024-08-18 16:45:21 +0300

    deleted unneeded imports

--- src/main/java/ru/agapov/springintegrationproject/SpringIntegrationProjectApplication.java
+++ src/main/java/ru/agapov/springintegrationproject/SpringIntegrationProjectApplication.java
@@ -1,14 +1,8 @@
 package ru.agapov.springintegrationproject;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.dsl.DirectChannelSpec;
-import org.springframework.integration.dsl.MessageChannels;
 import ru.agapov.springintegrationproject.services.MasterService;
 
 @SpringBootApplication
